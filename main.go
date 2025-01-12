4b1518b6bcf6c2f7577f93b185901acb95cf1f6e
JosephOkumu <jokumu25@gmail.com>
2025-01-12 05:13:58 +0300

    add login and register routes

--- main.go
+++ main.go
@@ -18,7 +18,8 @@ func main() {
 	defer db.Close() // Ensure database connection is closed
 
 	// Define routes
-
+	http.HandleFunc("/register", handlers.RegisterUserHandler) // User registration route
+    http.HandleFunc("/login", handlers.LoginHandler(db.DB))
 	// Start the server
 	fmt.Println("Server started on :8080")
 	err = http.ListenAndServe(":8080", nil)
