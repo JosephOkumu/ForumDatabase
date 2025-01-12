5379172b9d795ba3dfd48741bbfdfefee2a29c2a
JosephOkumu <jokumu25@gmail.com>
2025-01-12 05:13:37 +0300

    Initialize db

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"forum/db"
+	"forum/handlers"
+)
+
+func main() {
+	// Initialize the database
+	err := db.Initialize()
+	if err != nil {
+		log.Fatalf("Error initializing database: %v", err)
+	}
+	defer db.Close() // Ensure database connection is closed
+
+	// Define routes
+
+	// Start the server
+	fmt.Println("Server started on :8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+}
